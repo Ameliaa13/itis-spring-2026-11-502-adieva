c8b62a7e981084077d306d1325635a162549d93a
Amelka <ameliya.adiyeva@mail.ru>
2026-04-03 17:53:21 +0300

    homework

--- homeWork 28.03.2026/Finder.java
+++ homeWork 28.03.2026/Finder.java
@@ -7,7 +7,7 @@ public class Finder {
 
     public static List<String> solve(String file) throws IOException {
         List<String> result = new ArrayList<>();
-        Pattern pattern = Pattern.compile("\\b(WARN||ERROR||SEVERE)\\b");
+        Pattern pattern = Pattern.compile("\\b(WARN|ERROR|SEVERE)\\b");
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
